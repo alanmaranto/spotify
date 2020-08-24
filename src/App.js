ed4c7b975ab9aa7a512fca71fc44fbd681522db7
alan <alanarostegui@hotmail.com>
2020-08-23 20:59:23 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello</h1>
+      {/* Spotify Logo */}
+      {/* Login with spotify button */}
+    </div>
+  );
+}
+
+export default App;
