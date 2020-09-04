480f8b9723b47f777a78683acd4a73d86ae95fd1
alan <alanarostegui@hotmail.com>
2020-09-04 12:57:29 -0500

    feeature(sidebar):  add sidebar with options

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,10 +1,27 @@
 import React from "react";
+import SidebarOption from "./sidebarOptions/SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 import "./Sidebar.css";
 
 const Body = () => {
   return (
     <div className="sidebar">
-      <h1>Sidebar</h1>
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <SidebarOption title="Home" Icon={HomeIcon} />
+      <SidebarOption title="Search" Icon={SearchIcon} />
+      <SidebarOption title="Your Library" Icon={LibraryMusicIcon} />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr/>
+      <SidebarOption title="Trance" />
+      <SidebarOption title="Trance" />
+      <SidebarOption title="Trance" />
+      
     </div>
   );
 };
